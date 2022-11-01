00407faab1e13efbef1215fe843acde5cdd8d503
Easton Rayner <etr0012@tigermail.auburn.edu>
2022-11-01 18:39:44 -0500

    No real changes

---
+++ client.cpp
@@ -0,0 +1,1 @@
+// This program is intended to be run on the client side. It will initiate connection with a server and push a file using segmentation
---
+++ server.cpp
@@ -0,0 +1,1 @@
+// This program is intended to be run on the server side. It will wait for connection request from client and recieve a file, desegmenting it in the process
