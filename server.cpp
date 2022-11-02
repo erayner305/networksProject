c3cf1840f696064cfe7b29e4e5263eb1db8c8ad5
Garrett Dickinson <dickinsongarrett@gmail.com>
2022-11-01 23:50:05 -0500

    Finish prototyping packetization

--- server.cpp
+++ server.cpp
@@ -3,16 +3,20 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
-#include <string>
+#include <string.h>
+#include <stdio.h>
 
 int SEGMENT_SIZE = 512;
 int CHECKSUM_SIZE = 4;
-int DATA_SIZE = SEGMENT_SIZE - CHECKSUM_SIZE;
+int PACKET_COUNT_SIZE = 4;
+int HEADER_SIZE = CHECKSUM_SIZE + PACKET_COUNT_SIZE;
+int DATA_SIZE = SEGMENT_SIZE - HEADER_SIZE;
 
 void empty_buffer(char buffer[]);
 
 void generate_checksum(char input_buffer[], char output_buffer[]);
 
+void generate_packet_num(uint32_t packet_num, char packet_num_buffer[]);
 
 int main() {
 
@@ -40,26 +44,30 @@ int main() {
     // Define an empty buffer for our generated checksum
     char checksum_buffer[CHECKSUM_SIZE] = {};
 
+    // Define an empty buffer for our generated checksum
+    char packet_count_buffer[PACKET_COUNT_SIZE] = {};
+
     // Define our empty packet
     char packet[SEGMENT_SIZE] = {};
+
 
     while(!file_in.eof()){
         file_in.read(data_buffer, DATA_SIZE);
 
         // Generate our checksum using our buffer
         generate_checksum(data_buffer, checksum_buffer);
+        generate_packet_num(++packet_count, packet_count_buffer);
 
-        std::copy(checksum_buffer, checksum_buffer + CHECKSUM_SIZE, packet);
-        std::copy(data_buffer, data_buffer + DATA_SIZE, packet + CHECKSUM_SIZE);
+        std::copy(checksum_buffer, checksum_buffer+CHECKSUM_SIZE, packet);
+        std::copy(packet_count_buffer, packet_count_buffer+PACKET_COUNT_SIZE, packet+CHECKSUM_SIZE);
+        std::copy(data_buffer, data_buffer+DATA_SIZE, packet+8);
 
         // Implementation of packet here....
         for (int i = 0; i < SEGMENT_SIZE; i++) {
-            std::cout << (uint)packet[i] << std::endl;
+            std::cout << (uint8_t)packet[i] << std::endl;
         }
 
         empty_buffer(packet);
-
-        std::cout << std::endl << "End of Packet " << ++packet_count << std::endl;
     }
 
     file_in.close();
@@ -75,11 +83,9 @@ void empty_buffer(char buffer[]) {
 }
 
 
-void generate_packet_num(int packet_num, char packet_num_buffer[]) {
-    int shift_value = 16;
-    for (int i = 0; i < 8; i++) { 
-        packet_num_buffer[i] = (packet_num >> (shift_value -= 4)) & 0xFF; 
-    }
+void generate_packet_num(uint32_t packet_num, char packet_num_buffer[]) {
+    std::cout << "Packet Number: " << packet_num << std::endl;
+    memcpy(packet_num_buffer, &packet_num, sizeof(packet_num));
 }
 
 
@@ -88,11 +94,6 @@ void generate_checksum(char data_buffer[], char checksum_buffer[]) {
     for(int i = 0; i < DATA_SIZE; i++) {
         sum += data_buffer[i];
     }
-    std::cout << sum << std::endl;
-    int shift_value = 16;
-    for (int i = 0; i < 4; i++) { 
-        checksum_buffer[i] = (sum >> (shift_value -= 4)) & 0xFF;
-        std::cout << "shifted: " << ((sum >> (shift_value -= 4)) & 0xFF) << std::endl;
-        std::cout << "array: " << (uint)checksum_buffer[i] << std::endl;
-    }
+    std::cout << "Checksum: " << sum << std::endl;
+    memcpy(checksum_buffer, &sum, sizeof(sum));
 }
