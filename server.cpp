bc63518c742c93aefb4f58aff8684af5f8454367
Logan Sayle <logansayle@gmail.com>
2022-11-01 21:42:23 -0500

    checksum for server
    checksum function for server

--- server.cpp
+++ server.cpp
@@ -1,1 +1,18 @@
-// This program is intended to be run on the server side. It will wait for connection request from client and recieve a file, desegmenting it in the process
+#include <iostream>
+
+int main() {
+
+}
+
+char[] generate_checksum(char buffer[]) {
+    uint32_t sum = 0;
+    for(int i = 0; i < len(buffer); i++) {
+        sum += buffer[i];
+    }
+    char result[4];
+    shift_value = 32;
+    for (int i = 0; i < 8; i++) { 
+        result[i] = (var >> (shift_value -= 4)) & 0xF; 
+    }
+    return result
+}
