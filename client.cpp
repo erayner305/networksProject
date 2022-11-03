97d54f3ba854d0aa8710de1af645c9fed91d2da2
Garrett Dickinson <dickinsongarrett@gmail.com>
2022-11-02 20:35:08 -0500

    Fix up client bugs for server integration

--- client.cpp
+++ client.cpp
@@ -17,8 +17,7 @@ int main(int argc, char **argv) {
     int sd;
     struct sockaddr_in server;
     socklen_t serAddrLen;
-
-    std::string input_name;
+    serAddrLen = sizeof(server);
 
     sd = socket(AF_INET,SOCK_DGRAM,0);
 
@@ -26,61 +25,60 @@ int main(int argc, char **argv) {
     server.sin_port = htons(12345);
     server.sin_addr.s_addr = inet_addr("127.0.0.1");
 
-    while(true){
-        std::cout << "File name: " << std::flush;
-        std::getline(std::cin, input_name);
+    int n;
+    char message_buffer[512];
+    std::string input_filename;
+    std::ofstream downloaded_file;
 
-        char * input_name_cstr = new char [input_name.length()+1];
-        std::strcpy(input_name_cstr, input_name.c_str()); //convert from string to cstring
+    char file_data_buffer[504] = {};
+    char packet_instruction[4] = {};
 
-        char packet[input_name.length()+4] = {}; //create the packet that will be sent. +4 because string length, +1 for null, +3 for GET
+    while(true) {
+        std::cout << "File name to download: " << std::flush;
+        std::getline(std::cin, input_filename);
+
+        downloaded_file.open("received.bin");
+
+        char packet[512] = {};
         
         //populate "packet" with GET and the file name
-        packet[0] = 'G';
-        packet[1] = 'E';
-        packet[2] = 'T';
-        std::copy(input_name_cstr, input_name_cstr+input_name.length()+1, packet + 3);
+        strcpy("GET", packet);
+        memcpy(packet+4, input_filename.c_str(), sizeof(input_filename.c_str()));
         
         sendto(sd, packet, 1, 0, (struct sockaddr*)&server, sizeof(server));
 
-        //if file exists then break (needs implemented)
-        break;
-    }
-
-
-    int n;
-    char message_buffer[512];
-    serAddrLen = sizeof(server);
-    std::ofstream file;
-
-    file.open("received.bin");
-
-    char file_data_buffer[504] = {};
-
-    for (;;) {
-        std::cout << "Waiting for packet..." << std::endl;
-        
         n = recvfrom(sd, message_buffer, 512, 0, (struct sockaddr*)&server, &serAddrLen);
-        std::cout << "Got " << n << " bytes in response" << std::endl;
+        memcpy(packet_instruction, message_buffer, 4);
+        
+        if (packet_instruction == "ACK") {
+            // File exists
+            empty_buffer(message_buffer, 512);
 
-        std::memcpy(file_data_buffer, &message_buffer[8], 504);
-        file_data_buffer[504] = '\0';
+            while (message_buffer[0] != '\0') {
+                n = recvfrom(sd, message_buffer, 512, 0, (struct sockaddr*)&server, &serAddrLen);
+                std::cout << "[Info] Got " << n << " bytes in response" << std::endl;
 
-        size_t len = strlen(file_data_buffer); // will calculate number of non-0 symbols before first 0
-        char * newBuf = (char *)malloc(len); // allocate memory for new array, don't forget to free it later
-        memcpy(newBuf, file_data_buffer, len);
+                std::memcpy(file_data_buffer, &message_buffer[8], 504);
+                file_data_buffer[504] = '\0';
 
-        if (message_buffer[0] != '\0') {
-            file.write(newBuf, len);
+                size_t len = strlen(file_data_buffer); // will calculate number of non-0 symbols before first 0
+                char * newBuf = (char *)malloc(len); // allocate memory for new array, don't forget to free it later
+                memcpy(newBuf, file_data_buffer, len);
+
+                downloaded_file.write(newBuf, len);
+            }
+            
+            std::cout << "[Info] End of transmission" << std::endl;
+            
         } else {
-            std::cout << "End of transmission" << std::endl;
-            file.close();
+            std::cout << "[Error] File name does not exist on server, please try again" << std::endl;
         }
 
+        downloaded_file.close();
+
         empty_buffer(file_data_buffer, 504);
-        empty_buffer(message_buffer, 512);
     }
-    
+
     return 0;
 }
 
